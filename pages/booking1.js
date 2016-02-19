d3c8ca4b7ae2dbc8e50479b80d94db9f22bb47a5
Eric Lu <ericluwj@hotmail.com>
2016-02-19 12:39:50 +0800

    Set path as prop into pages as well.

--- pages/booking1.js
+++ pages/booking1.js
@@ -10,7 +10,7 @@ export default class extends Component {
     return (
       <div>
         <div>
-          <BookingNavigation />
+          <BookingNavigation path={this.props.path} />
           <BookingServices />          
         </div>
       </div>

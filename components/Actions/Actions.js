ecbf4a02573a5fe6763089638731a0b840531b9a
Eric Lu <ericluwj@hotmail.com>
2016-03-10 17:19:41 +0800

    Fixed booking button in Actions component.

--- components/Actions/Actions.js
+++ components/Actions/Actions.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Actions.scss';
 import Container from '../Container';
+import Link from '../Link';
 
 export default class Features extends Component {
 
@@ -12,12 +13,12 @@ export default class Features extends Component {
             <div className="Actions-item">
               <img src={require('./actions-1.png')} />
               <div className="Actions-item-title">Book A Caregiver Online</div>
-              <a href="" className="btn Actions-item-button">Book A Caregiver</a>
+              <a href="/booking1" className="btn Actions-item-button" onClick={Link.handleClick}>Book A Caregiver</a>
             </div>
             <div className="Actions-item">
               <img src={require('./actions-2.png')} />
               <div className="Actions-item-title">Call Ebeecare Hotline</div>
-              <a href="#" className="btn Actions-item-button" disabled={true}>9733 6938</a>
+              <a href="#" className="btn Actions-item-button" onClick={(e) => {e.preventDefault()}}>9733 6938</a>
             </div>
           </div>
         </Container>

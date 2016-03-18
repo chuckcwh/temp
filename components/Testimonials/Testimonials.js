d2327b5165c74e6af50c06d5ea3764e96978192d
Eric Lu <ericluwj@hotmail.com>
2016-03-18 12:51:48 +0800

    Added testimonials component.

--- components/Testimonials/Testimonials.js
+++ components/Testimonials/Testimonials.js
@@ -15,27 +15,51 @@ export default class Testimonials extends Component {
           <div className="Testimonials-list">
             <div className="Testimonials-item">
               <div className="Testimonials-wrapper">
-                <div className="Testimonials-item-text">Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</div>
-                <div className="Testimonials-item-name">Mr. Goh Mun</div>
-                <div className="Testimonials-item-avatar" dangerouslySetInnerHTML={{__html: avatar('https://graph.facebook.com/574555853/picture?width=240&height=240')}}>
+                <div className="Testimonials-item-text">I tried out a few nutritionists from eBeeCare (for free) before I found Mark (a nutritionist whom I am now engaging on a long term basis).</div>
+                <div className="Testimonials-item-name">Vietnamese Businessman<br />Mr Ho Qun Tea</div>
+                <div className="Testimonials-item-avatar">
+                  <svg width="120" height="120" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1">
+                    <g>
+                      <clipPath id="hex-mask">
+                        <polygon fill="#ff0000" points="60,0 120,30 120,90 60,120 0,90 0,30" />
+                      </clipPath>
+                    </g>
+                    <image clipPath="url(#hex-mask)" height="120" width="120" xlinkHref={require('./ho_qun_tea.png')} />
+                  </svg>
                 </div>
               </div>
               <div></div>
             </div>
             <div className="Testimonials-item">
               <div className="Testimonials-wrapper">
-                <div className="Testimonials-item-text">Nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</div>
-                <div className="Testimonials-item-name">Ms. Sherly Li</div>
-                <div className="Testimonials-item-avatar" dangerouslySetInnerHTML={{__html: avatar('https://graph.facebook.com/100010858146192/picture?width=240&height=240')}}>
+                <div className="Testimonials-item-text">We never knew wound care procedure could be done at home! Thanks to eBeeCare, now we do not need to take leave to bring my mum to hospital for simple wound dressing.</div>
+                <div className="Testimonials-item-name">Daughter of<br />Madam Goh Chng Ying</div>
+                <div className="Testimonials-item-avatar">
+                  <svg width="120" height="120" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1">
+                    <g>
+                      <clipPath id="hex-mask">
+                        <polygon fill="#ff0000" points="60,0 120,30 120,90 60,120 0,90 0,30" />
+                      </clipPath>
+                    </g>
+                    <image clipPath="url(#hex-mask)" height="120" width="120" xlinkHref={require('./goh_chng_ying.png')} />
+                  </svg>
                 </div>
               </div>
               <div></div>
             </div>
             <div className="Testimonials-item">
               <div className="Testimonials-wrapper">
-                <div className="Testimonials-item-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut </div>
-                <div className="Testimonials-item-name">Mrs. Erika Tan</div>
-                <div className="Testimonials-item-avatar" dangerouslySetInnerHTML={{__html: avatar('https://graph.facebook.com/100010744992918/picture?width=240&height=240')}}>
+                <div className="Testimonials-item-text">It is so convenient to get a professional nurse from eBeeCare, even for last minute requests; and their price is very competitive.</div>
+                <div className="Testimonials-item-name">School Trip Organizer<br />Ms Cindy Chng</div>
+                <div className="Testimonials-item-avatar">
+                  <svg width="120" height="120" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1">
+                    <g>
+                      <clipPath id="hex-mask">
+                        <polygon fill="#ff0000" points="60,0 120,30 120,90 60,120 0,90 0,30" />
+                      </clipPath>
+                    </g>
+                    <image clipPath="url(#hex-mask)" height="120" width="120" xlinkHref={require('./cindy_chng.png')} />
+                  </svg>
                 </div>
               </div>
               <div></div>

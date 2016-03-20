c4b3dfd0b783e7f0748df4bc6bdfa83212898d5b
Eric Lu <ericluwj@hotmail.com>
2016-03-20 23:45:10 +0800

    Amended copywriting in booking nav menu.

--- components/BookingNavigation/BookingNavigation.js
+++ components/BookingNavigation/BookingNavigation.js
@@ -10,7 +10,7 @@ export default class BookingNavigation extends Component {
       <div className="BookingNavigation-wrapper">
         <Container>
           <ul className="BookingNavigation">
-            <BookingNavigationItem path={this.props.path} active={'booking1'} link={'booking1'} icon={'1'} text={'Select Services'} />
+            <BookingNavigationItem path={this.props.path} active={'booking1'} link={'booking1'} icon={'1'} text={'Select Service'} />
             <BookingNavigationItem path={this.props.path} active={'booking2'} link={'booking2'} icon={'2'} text={'Select Location'} />
             <BookingNavigationItem path={this.props.path} active={'booking3'} link={'booking3a'} icon={'3'} text={'Select Date & Time'} />
           </ul>

f78151624a929b31fed2652da8ab7b550474b70e
Eric Lu <ericluwj@hotmail.com>
2016-04-04 10:19:44 +0800

    Updated config.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-  title: 'eBeecare',
+  title: 'eBeeCare',
   description: 'eBeeCare - Homecare booking platform to help you book and manage healthcare services for loved ones in an easy way.',
   googleAnalyticsId: 'UA-73367021-1',
 };

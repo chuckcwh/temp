dbdcd906da348a187738e503be7f8d1bf0b49ff2
Eric Lu <ericluwj@hotmail.com>
2016-04-06 10:56:02 +0800

    Patched case URL to using Util backend.

--- components/BookingComplete/BookingComplete.js
+++ components/BookingComplete/BookingComplete.js
@@ -143,7 +143,7 @@ export default class BookingComplete extends Component {
           <div>
             <b>CASE ID : {this.state.caseId}</b>
             <div>
-              <a href={'https://app.ebeecare.com/cases/' + this.state.caseId}>View Case</a>
+              <a href={Util.backend + '/cases/' + this.state.caseId}>View Case</a>
             </div>
           </div>
         );

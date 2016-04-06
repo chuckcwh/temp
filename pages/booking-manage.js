cbaf7f990228be6b33244de62fb68565a76c2dc7
Eric Lu <ericluwj@hotmail.com>
2016-04-06 10:39:41 +0800

    Patched booking-manage page.

--- pages/booking-manage.js
+++ pages/booking-manage.js
@@ -1,48 +1,12 @@
 import React, { Component } from 'react';
-import Account from '../components/Account/Account';
-import BookingDetails from '../components/BookingDetails/BookingDetails';
-import BookingStore from '../stores/BookingStore';
+import BookingApp from '../components/BookingApp/BookingApp';
 
 export default class extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      booking: undefined
-    };
-  }
-
-  componentDidMount() {
-    BookingStore.addChangeListener(this._onChange.bind(this));
-  }
-
-  componentWillUnmount() {
-    BookingStore.removeChangeListener(this._onChange.bind(this));
-  }
-  
   render() {
-    var component;
-    if (this.state.booking && this.state.booking.id) {
-      component = (
-        <BookingDetails booking={this.state.booking} />
-      );
-    } else {
-      component = (
-        <Account type="login" />
-      );
-    }
     return (
-      <div>
-        {component}
-      </div>
+      <BookingApp location={this.props.location} path={this.props.path} />
     );
   }
 
-  /**
-   * Event handler for 'change' events coming from the BookingStore
-   */
-  _onChange() {
-    this.setState(BookingStore.getState());
-    // console.log(this.state);
-  }
 }

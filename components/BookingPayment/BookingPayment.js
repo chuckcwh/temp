d614ee8f841421eed6ea6a647b821a63aa6a8e2b
Eric Lu <ericluwj@hotmail.com>
2016-04-14 17:42:17 +0800

    Enabled BookingPayment module to retrieve booking and check whether there are 3 days before.

--- components/BookingPayment/BookingPayment.js
+++ components/BookingPayment/BookingPayment.js
@@ -8,6 +8,29 @@ import BookingActions from '../../actions/BookingActions';
 export default class BookingPayment extends Component {
 
   render() {
+    var bankTransferItem;
+    var dates = this.props.booking && this.props.booking.case && this.props.booking.case.dates;
+    if (dates) {
+      var earliestDate;
+      for (var i = 0; i < dates.length; i++) {
+        if (earliestDate) {
+          var d = new Date(dates[i]['dateTimeStart']);
+          if (earliestDate > d) {
+            earliestDate = d;
+          }
+        } else {
+          earliestDate = new Date(dates[i]['dateTimeStart']);
+        }
+      }
+      earliestDate.setDate(earliestDate.getDate() - 3);
+      if (earliestDate > new Date()) {
+        bankTransferItem = (
+          <li className="BookingPaymentNav-item">
+            <a className={classNames('BookingPaymentNav-link', (this.props.path === '/booking-confirmation' && this.props.postStatus === 'payment-bank') ? 'active' : '')} href="#" onClick={this._onClick.bind(this, 'bank')}>Bank Transfer<span className="BookingPaymentNav-arrow"><div className="nav-caret"></div></span></a>
+          </li>
+        );
+      }
+    }
     return (
       <div className="BookingPayment">
         <div className="BookingPaymentNav-wrapper">
@@ -16,9 +39,7 @@ export default class BookingPayment extends Component {
               <li className="BookingPaymentNav-item">
                 <a className={classNames('BookingPaymentNav-link', (this.props.path === '/booking-confirmation' && this.props.postStatus === 'payment-paypal') ? 'active' : '')} href="#" onClick={this._onClick.bind(this, 'paypal')}>Paypal (Credit/Debit)<span className="BookingPaymentNav-arrow"><div className="nav-caret"></div></span></a>
               </li>
-              <li className="BookingPaymentNav-item">
-                <a className={classNames('BookingPaymentNav-link', (this.props.path === '/booking-confirmation' && this.props.postStatus === 'payment-bank') ? 'active' : '')} href="#" onClick={this._onClick.bind(this, 'bank')}>Bank Transfer<span className="BookingPaymentNav-arrow"><div className="nav-caret"></div></span></a>
-              </li>
+              {bankTransferItem}
               {/*<li className="BookingPaymentNav-item">
                 <a className={classNames('BookingPaymentNav-link', (this.props.path === '/booking-confirmation' && this.props.postStatus === 'payment-credits') ? 'active' : '')} href="#" onClick={this._onClick.bind(this, 'credits')}>eBeeCare Credits<span className="BookingPaymentNav-arrow"><div className="nav-caret"></div></span></a>
               </li>*/}

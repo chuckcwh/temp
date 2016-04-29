fde8c0b9ef4c16a3e89e0618b87c77215992b6df
Eric Lu <ericluwj@hotmail.com>
2016-04-29 12:49:13 +0800

    Implemented slideshow and revamped slides.

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Slider from 'react-slick';
 import classNames from 'classnames';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './Banner.scss';
@@ -12,27 +13,28 @@ export default class Banner extends Component {
     super(props);
     this.state = {
       bgImageIndex: 0,
-      bgImages: []
+      bgImages: [0]
     };
   }
 
   componentDidMount() {
-    this._startSlideshow();
+    // this._startSlideshow();
   }
 
   render() {
     return (
       <div className="Banner">
-        <ReactCSSTransitionGroup transitionName="Banner-bg" transitionEnterTimeout={1000} transitionLeaveTimeout={1000}>
-          {
-            this.state.bgImages.map((i) => {
-              return (
-                <div className={classNames('Banner-bg', 'Banner-bg-' + i)} key={'Banner-bg-' + i} />
-              );
-            })
-          }
-        </ReactCSSTransitionGroup>
-        <div className={classNames('Banner-item', 'Banner-item-' + this.state.bgImageIndex)} id="Banner-item-1">
+        <div className="SliderWrapper">
+          <Slider dots={true} infinite={true} speed={1000} autoplay={true} autoplaySpeed={10000} slidesToShow={1} slidesToScroll={1}>
+            <div className={classNames('Banner-bg', 'Banner-bg-0')}>
+              <div className="Banner-bg-text">GERIATRIC CARE</div>
+            </div>
+            <div className={classNames('Banner-bg', 'Banner-bg-1')}>
+              <div className="Banner-bg-text">MOTHER CARE</div>
+            </div>
+          </Slider>
+        </div>
+        <div className="Banner-item" id="Banner-item-1">
           <div className="Banner-item-text-wrapper text-center">
             <div className="Banner-item-text Banner-item-text-1">The Best Homecare Option</div>
             <div className="Banner-item-text Banner-item-text-2">Family Caregivers</div>

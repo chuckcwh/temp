c075c9534ee50ae7b281226662677c1c00556bc9
Eric Lu <ericluwj@hotmail.com>
2016-05-27 13:14:31 +0800

    Added new close button component.

---
+++ components/CloseButton/CloseButton.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './CloseButton.scss';
+
+class CloseButton extends Component {
+
+  render() {
+    return (
+      <a onClick={() => this.onCloseClicked()} role="button" className="CloseButton">&times;</a>
+    );
+  }
+}
+
+CloseButton.displayName = 'CloseButton';
+
+CloseButton.propTypes = {
+  onCloseClicked: React.PropTypes.func
+};
+
+export default CloseButton;

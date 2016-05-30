d739325a9dd85481b0148aab69cd6dbac6184eb8
Eric Lu <ericluwj@hotmail.com>
2016-05-30 18:06:40 +0800

    Converted to relying use of NODE_ENV variable to determine store configured.

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,6 +1,4 @@
-const DEBUG = !process.argv.includes('release');
-
-if (!DEBUG) {
+if (process.env.NODE_ENV === 'production') {
   module.exports = require('./configureStore.prod')
 } else {
   module.exports = require('./configureStore.dev')

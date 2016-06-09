c1eecaccc72fbf30427245b2e4275cd8897ab20b
Eric Lu <ericluwj@hotmail.com>
2016-06-09 17:56:07 +0800

    Amended multiselect component.

--- components/MultiSelect/MultiSelect.js
+++ components/MultiSelect/MultiSelect.js
@@ -7,13 +7,7 @@ class MultiSelect extends Component {
   render() {
     const { onBlur, onChange, options, value, ...rest } = this.props
     const parse = event => {
-      // const result = [];
-      // // event.target.selectedOptions is a NodeList, not an array. Gross.
-      // for (let index = 0; index < event.target.selectedOptions.length; index++) {
-      //   result.push(event.target.selectedOptions[index].value);
-      // }
-      // return result;
-      console.log(event);
+      // console.log(event);
       return event;
     }
     const val = Array.isArray(value) ? value.map(JSON.stringify) : JSON.stringify(value)

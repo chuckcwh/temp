7b9a39f6b4cef1d739fb0531d60f038628e44a6d
Eric Lu <ericluwj@hotmail.com>
2016-06-16 11:47:05 +0800

    Revamped features section.

--- components/Features/Features.js
+++ components/Features/Features.js
@@ -12,24 +12,58 @@ export default class Features extends Component {
           <p className="text-center featured">eBeeCare is the largest online local home healthcare provider in Singapore. Since 2015 our caregivers have provided over 50,000 hours of care for patients... and they are ready to serve you!</p>
           <div className="Features-list">
             <div className="Features-item">
-              <img src={require('./features-1.png')} className="Features-item-icon" />
-              <div className="Features-item-title">Short-hours care</div>
-              <div>No minimum hours required!</div>
+              <div className="Features-item-left">
+                <img src={require('./features-1.png')} className="Features-item-icon" />
+              </div>
+              <div className="Features-item-right">
+                <div className="Features-item-title">Interview</div>
+                <div>By both eBeeCare and community</div>
+              </div>
             </div>
             <div className="Features-item">
-              <img src={require('./features-2.png')} className="Features-item-icon" />
-              <div className="Features-item-title">Hassle-Free</div>
-              <div>Let us handle the coordination for you!</div>
+              <div className="Features-item-left">
+                <img src={require('./features-2.png')} className="Features-item-icon" />
+              </div>
+              <div className="Features-item-right">
+                <div className="Features-item-title">Certification</div>
+                <div>By both eBeeCare and community</div>
+              </div>
             </div>
             <div className="Features-item">
-              <img src={require('./features-3.png')} className="Features-item-icon" />
-              <div className="Features-item-title">Customised Care</div>
-              <div>Every patient has different needs</div>
+              <div className="Features-item-left">
+                <img src={require('./features-3.png')} className="Features-item-icon" />
+              </div>
+              <div className="Features-item-right">
+                <div className="Features-item-title">Experience</div>
+                <div>By both eBeeCare and community</div>
+              </div>
             </div>
             <div className="Features-item">
-              <img src={require('./features-4.png')} className="Features-item-icon" />
-              <div className="Features-item-title">Verified Caregivers</div>
-              <div>By both eBeeCare and community</div>
+              <div className="Features-item-left">
+                <img src={require('./features-4.png')} className="Features-item-icon" />
+              </div>
+              <div className="Features-item-right">
+                <div className="Features-item-title">Affordable Quality</div>
+                <div>Every patient has different needs</div>
+              </div>
+            </div>
+            <div className="Features-item">
+              <div className="Features-item-left">
+                <img src={require('./features-5.png')} className="Features-item-icon" />
+              </div>
+              <div className="Features-item-right">
+                <div className="Features-item-title">Convenience</div>
+                <div>No minimum hours required!</div>
+              </div>
+            </div>
+            <div className="Features-item">
+              <div className="Features-item-left">
+                <img src={require('./features-6.png')} className="Features-item-icon" />
+              </div>
+              <div className="Features-item-right">
+                <div className="Features-item-title">Safety Assurance</div>
+                <div>Let us handle the coordination for you!</div>
+              </div>
             </div>
           </div>
         </Container>

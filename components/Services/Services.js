d34f4d4cb6b34e6401abe86517b72e4e9fd45da3
Eric Lu <ericluwj@hotmail.com>
2016-06-24 18:25:32 +0800

    Segregated services page.

--- components/Services/Services.js
+++ components/Services/Services.js
@@ -25,21 +25,18 @@ class Services extends Component {
   }
 
   render() {
-    const { allServices, allServicesFetching } = this.props;
+    const { location, allServices, allServicesFetching } = this.props;
     const { filter } = this.state;
 
     const serviceTree = Util.appendAllServices(Util.parseCategories(allServices));
     let serviceTreeHash = {};
     serviceTree.map(category => { serviceTreeHash[category.name] = category });
 
-    return (
-      <div className="Services">
-        <Container>
-          <div>
-            <h1 className="text-center">Services</h1>
-          </div>
-        </Container>
-        <Loader className="spinner" loaded={allServicesFetching ? false : true}>
+    let serviceContent;
+    if (location.query && location.query.subcat && allServices) {
+      let subcat = parseInt(location.query.subcat);
+      serviceContent = (
+        <div>
           <div className="ServicesNav-wrapper">
             <Container>
               <ul className="ServicesNav">
@@ -90,6 +87,74 @@ class Services extends Component {
               </div>
             </Container>
           </div>
+        </div>
+      );
+    } else {
+      serviceContent = (
+        <div>
+          <div className="ServicesNav-wrapper">
+            <Container>
+              <ul className="ServicesNav">
+              {
+                serviceTree.map(category => {
+                  const { name } = category;
+                  return (
+                    <li className="ServicesNav-item" key={name}>
+                      <a className={classNames('ServicesNav-link', (filter === name) ? 'active' : '')} href="#" onClick={this._onClickFilter.bind(this, name)}>{name}<span className="ServicesNav-arrow"><div className="nav-caret"></div></span></a>
+                    </li>
+                  );
+                })
+              }
+              </ul>
+            </Container>
+          </div>
+          <div>
+            <Container>
+              <div className="ServicesBody">
+                {
+                  serviceTreeHash[filter].children.map(subType => {
+                    return (
+                      <div key={subType.children[0].category + subType.name}>
+                        <h3>{this.state.filter === Util.ALL_SERVICES ? subType.children[0].category + ' > ' : ''}{subType.name}</h3>
+                        <Accordion activeItems={-1}>
+                          {
+                            subType.children.map((service) => {
+                              return (
+                                <AccordionItem title={service.name} key={service.id}>
+                                  <div className="ServicesItem">
+                                    <div className="ServicesItemDescription">
+                                      {service.description} ({parseFloat(service.duration)} hours)<br />
+                                      <span className="ServicesItemDescription-price">Starting from SGD {service.price} per session</span>
+                                    </div>
+                                    <div>
+                                      <button className="btn btn-primary btn-small" onClick={this._onClickBook.bind(this, service)}>Book Service</button>
+                                    </div>
+                                  </div>
+                                </AccordionItem>
+                              );
+                            })
+                          }
+                        </Accordion>
+                      </div>
+                    );
+                  })
+                }
+              </div>
+            </Container>
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <div className="Services">
+        <Container>
+          <div>
+            <h1 className="text-center">Services</h1>
+          </div>
+        </Container>
+        <Loader className="spinner" loaded={allServicesFetching ? false : true}>
+          {serviceContent}
         </Loader>
       </div>
     );
@@ -113,6 +178,7 @@ class Services extends Component {
 
 const mapStateToProps = (state) => {
   return {
+    location: state.router && state.router.location,
     allServices: state.allServices.data,
     allServicesFetching: state.allServices.isFetching
   }
@@ -121,7 +187,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchServices: () => {
-      dispatch(fetchServices());
+      return dispatch(fetchServices());
     }
   }
 }

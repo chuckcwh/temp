df113df95fe39c41133a635f7efcca84a1a98a5a
Eric Lu <ericluwj@hotmail.com>
2016-08-15 18:30:51 +0800

    Revised DashboardTableButton.

--- components/DashboardTableButton/DashboardTableButton.js
+++ components/DashboardTableButton/DashboardTableButton.js
@@ -1,25 +1,40 @@
 import React from 'react';
 import classNames from 'classnames';
 import s from './DashboardTableButton.css';
+import Link from '../Link';
 
-const DashboardTableButton = (props) => (
-  <button
-    onClick={props.onClick}
-    role="button"
-    className={classNames(s.dashboardTableButton, s[props.color])}
-  >
-    {props.children}
-  </button>
-);
+const DashboardTableButton = (props) => {
+  if (props.to) {
+    return (
+      <Link
+        to={props.to}
+        className={classNames(s.dashboardTableButton, s[props.color])}
+      >
+        {props.children}
+      </Link>
+    );
+  } else {
+    return (
+      <button
+        onClick={props.onClick}
+        role="button"
+        className={classNames(s.dashboardTableButton, s[props.color])}
+      >
+        {props.children}
+      </button>
+    );
+  }
+};
 
 DashboardTableButton.displayName = 'DashboardTableButton';
 
 DashboardTableButton.propTypes = {
   children: React.PropTypes.node,
 
-  color: React.PropTypes.string.isRequired,
+  color: React.PropTypes.string,
 
   onClick: React.PropTypes.func,
+  to: React.PropTypes.string,
 };
 
 DashboardTableButton.defaultProps = {

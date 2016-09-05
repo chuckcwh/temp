f2e1095baf6b967bd47124f8be754c70c8cab79f
Eric Lu <ericluwj@hotmail.com>
2016-09-05 13:22:20 +0800

    Fixed user reducer.

--- reducers/user.js
+++ reducers/user.js
@@ -5,6 +5,16 @@ import { isClient } from '../core/util';
 
 const devices = (state = null, action) => {
   switch (action.type) {
+    case ActionTypes.USER_SUCCESS:
+    case ActionTypes.USER_TOKEN_SUCCESS:
+    case ActionTypes.USER_CREATE_SUCCESS:
+    case ActionTypes.LOGIN_SUCCESS:
+    case ActionTypes.LOGIN_CLIENT_SUCCESS:
+    case ActionTypes.USER_EDIT_SUCCESS:
+      return Object.assign({}, state, action.response.data && action.response.data.devices && action.response.data.devices.reduce((result, elem) => {
+        result[elem._id] = elem;
+        return result;
+      }, {}));
     case ActionTypes.USER_DEVICES_SUCCESS:
       return Object.assign({}, state, action.response.data.reduce((result, elem) => {
         result[elem._id] = elem;
@@ -23,6 +33,16 @@ const devices = (state = null, action) => {
 
 const experiences = (state = null, action) => {
   switch (action.type) {
+    case ActionTypes.USER_SUCCESS:
+    case ActionTypes.USER_TOKEN_SUCCESS:
+    case ActionTypes.USER_CREATE_SUCCESS:
+    case ActionTypes.LOGIN_SUCCESS:
+    case ActionTypes.LOGIN_CLIENT_SUCCESS:
+    case ActionTypes.USER_EDIT_SUCCESS:
+      return Object.assign({}, state, action.response.data && action.response.data.experiences && action.response.data.experiences.reduce((result, elem) => {
+        result[elem._id] = elem;
+        return result;
+      }, {}));
     case ActionTypes.USER_EXPERIENCES_SUCCESS:
       return Object.assign({}, state, action.response.data.reduce((result, elem) => {
         result[elem._id] = elem;
@@ -41,6 +61,16 @@ const experiences = (state = null, action) => {
 
 const educations = (state = null, action) => {
   switch (action.type) {
+    case ActionTypes.USER_SUCCESS:
+    case ActionTypes.USER_TOKEN_SUCCESS:
+    case ActionTypes.USER_CREATE_SUCCESS:
+    case ActionTypes.LOGIN_SUCCESS:
+    case ActionTypes.LOGIN_CLIENT_SUCCESS:
+    case ActionTypes.USER_EDIT_SUCCESS:
+      return Object.assign({}, state, action.response.data && action.response.data.educations && action.response.data.educations.reduce((result, elem) => {
+        result[elem._id] = elem;
+        return result;
+      }, {}));
     case ActionTypes.USER_EDUCATIONS_SUCCESS:
       return Object.assign({}, state, action.response.data.reduce((result, elem) => {
         result[elem._id] = elem;
@@ -59,6 +89,16 @@ const educations = (state = null, action) => {
 
 const achievements = (state = null, action) => {
   switch (action.type) {
+    case ActionTypes.USER_SUCCESS:
+    case ActionTypes.USER_TOKEN_SUCCESS:
+    case ActionTypes.USER_CREATE_SUCCESS:
+    case ActionTypes.LOGIN_SUCCESS:
+    case ActionTypes.LOGIN_CLIENT_SUCCESS:
+    case ActionTypes.USER_EDIT_SUCCESS:
+      return Object.assign({}, state, action.response.data && action.response.data.achievements && action.response.data.achievements.reduce((result, elem) => {
+        result[elem._id] = elem;
+        return result;
+      }, {}));
     case ActionTypes.USER_ACHIEVEMENTS_SUCCESS:
       return Object.assign({}, state, action.response.data.reduce((result, elem) => {
         result[elem._id] = elem;
@@ -77,6 +117,16 @@ const achievements = (state = null, action) => {
 
 const reviews = (state = null, action) => {
   switch (action.type) {
+    case ActionTypes.USER_SUCCESS:
+    case ActionTypes.USER_TOKEN_SUCCESS:
+    case ActionTypes.USER_CREATE_SUCCESS:
+    case ActionTypes.LOGIN_SUCCESS:
+    case ActionTypes.LOGIN_CLIENT_SUCCESS:
+    case ActionTypes.USER_EDIT_SUCCESS:
+      return Object.assign({}, state, action.response.data && action.response.data.reviews && action.response.data.reviews.reduce((result, elem) => {
+        result[elem._id] = elem;
+        return result;
+      }, {}));
     case ActionTypes.USER_REVIEWS_SUCCESS:
       return Object.assign({}, state, action.response.data.reduce((result, elem) => {
         result[elem._id] = elem;
@@ -95,6 +145,16 @@ const reviews = (state = null, action) => {
 
 const schedules = (state = null, action) => {
   switch (action.type) {
+    case ActionTypes.USER_SUCCESS:
+    case ActionTypes.USER_TOKEN_SUCCESS:
+    case ActionTypes.USER_CREATE_SUCCESS:
+    case ActionTypes.LOGIN_SUCCESS:
+    case ActionTypes.LOGIN_CLIENT_SUCCESS:
+    case ActionTypes.USER_EDIT_SUCCESS:
+      return Object.assign({}, state, action.response.data && action.response.data.schedules && action.response.data.schedules.reduce((result, elem) => {
+        result[elem._id] = elem;
+        return result;
+      }, {}));
     case ActionTypes.USER_SCHEDULES_SUCCESS:
       return Object.assign({}, state, action.response.data.reduce((result, elem) => {
         result[elem._id] = elem;
@@ -110,7 +170,7 @@ const schedules = (state = null, action) => {
       return state;
   }
 }
-const userData = combineReducers({
+const partialUserData = combineReducers({
   devices,
   experiences,
   educations,
@@ -145,7 +205,23 @@ const extendedUserData = (state, action) => {
     case ActionTypes.USER_SCHEDULE_SUCCESS:
     case ActionTypes.USER_SCHEDULE_CREATE_SUCCESS:
     case ActionTypes.USER_SCHEDULE_EDIT_SUCCESS:
-      return Object.assign({}, state, userData(state, action));
+      return Object.assign({}, state, partialUserData(state, action));
+    default:
+      return state;
+  }
+}
+
+const fullUserData = (state, action) => {
+  switch(action.type) {
+    case ActionTypes.USER_SUCCESS:
+    case ActionTypes.USER_TOKEN_SUCCESS:
+    case ActionTypes.USER_CREATE_SUCCESS:
+    case ActionTypes.LOGIN_SUCCESS:
+    case ActionTypes.USER_EDIT_SUCCESS:
+      return Object.assign({}, state, action.response.data, partialUserData(state, action));
+    case ActionTypes.LOGIN_CLIENT_SUCCESS:
+      return Object.assign({}, state, (isClient(action.response.data) ? action.response.data : null),
+      (isClient(action.response.data) ? partialUserData(state, action) : null));
     default:
       return state;
   }
@@ -179,14 +255,25 @@ const user = (state = {
       return Object.assign({}, state, {
         isFetching: false,
         didInvalidate: false,
-        data: action.response && action.response.data,
+        data: fullUserData(state, action),
+        lastUpdated: action.response && action.response.receivedAt
+      })
+    case ActionTypes.LOGIN_CLIENT_SUCCESS:
+      if (action.response && action.response.data && action.response.data._id && action.response.token) {
+        cookie.save('user_id', action.response.data._id, { path: '/' });
+        cookie.save('user_token', action.response.token, { path: '/' });
+      }
+      return Object.assign({}, state, {
+        isFetching: false,
+        didInvalidate: true,
+        data: fullUserData(state, action),
         lastUpdated: action.response && action.response.receivedAt
       })
     case ActionTypes.USER_EDIT_SUCCESS:
       return Object.assign({}, state, {
         isFetching: false,
         didInvalidate: false,
-        data: action.response && action.response.data,
+        data: fullUserData(state, action),
         lastUpdated: action.response && action.response.receivedAt
       })
     case ActionTypes.USER_DEVICES_SUCCESS:
@@ -218,17 +305,6 @@ const user = (state = {
         didInvalidate: false,
         data: extendedUserData(state, action),
         lastUpdated: action.response && action.response.receiveAt
-      })
-    case ActionTypes.LOGIN_CLIENT_SUCCESS:
-      if (action.response && action.response.data && action.response.data._id && action.response.token) {
-        cookie.save('user_id', action.response.data._id, { path: '/' });
-        cookie.save('user_token', action.response.token, { path: '/' });
-      }
-      return Object.assign({}, state, {
-        isFetching: false,
-        didInvalidate: true,
-        data: (action.response && action.response.data && isClient(action.response.data)) ? action.response.data : null,
-        lastUpdated: action.response && action.response.receivedAt
       })
     case ActionTypes.USER_TOKEN_FAILURE:
       cookie.remove('user_id', { path: '/' });

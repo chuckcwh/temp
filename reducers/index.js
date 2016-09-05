a8aa932cf17371b9b398abdd4f92119517338895
Eric Lu <ericluwj@hotmail.com>
2016-09-05 10:44:26 +0800

    Fixed user reducer.

--- reducers/index.js
+++ reducers/index.js
@@ -3,83 +3,13 @@ import cookie from 'react-cookie';
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import merge from 'lodash/merge';
+import user from './user';
 import modal from './modal';
 import order from './order';
-import { appendAllServices, parseCategories, isClient } from '../core/util';
+import userData from './user';
+import { appendAllServices, parseCategories } from '../core/util';
 import sortBy from 'lodash/sortBy';
 import moment from 'moment';
-
-const user = (state = {
-  isFetching: false,
-  didInvalidate: true,
-  data: null
-}, action) => {
-  switch (action.type) {
-    case ActionTypes.USER_REQUEST:
-    case ActionTypes.USER_TOKEN_REQUEST:
-    case ActionTypes.USER_CREATE_REQUEST:
-    case ActionTypes.USER_EDIT_REQUEST:
-    case ActionTypes.USER_EDUCATION_EDIT_REQUEST:
-    case ActionTypes.LOGIN_REQUEST:
-    case ActionTypes.LOGIN_CLIENT_REQUEST:
-      return Object.assign({}, state, {
-        isFetching: true,
-        didInvalidate: false
-      })
-    case ActionTypes.USER_SUCCESS:
-    case ActionTypes.USER_TOKEN_SUCCESS:
-    case ActionTypes.USER_CREATE_SUCCESS:
-    case ActionTypes.LOGIN_SUCCESS:
-      if (action.response && action.response.data && action.response.data._id && action.response.token) {
-        cookie.save('user_id', action.response.data._id, { path: '/' });
-        cookie.save('user_token', action.response.token, { path: '/' });
-      }
-      return Object.assign({}, state, {
-        isFetching: false,
-        didInvalidate: false,
-        data: action.response && action.response.data,
-        lastUpdated: action.response && action.response.receivedAt
-      })
-    case ActionTypes.USER_EDIT_SUCCESS:
-      return Object.assign({}, state, {
-        isFetching: false,
-        didInvalidate: false,
-        data: action.response && action.response.data,
-        lastUpdated: action.response && action.response.receivedAt
-      })
-    case ActionTypes.USER_EDUCATION_EDIT_SUCCESS:
-      return Object.assign({}, state, {
-        isFetching: false,
-        didInvalidate: false,
-        data: action.response && action.response.data,
-        lastUpdated: action.response && action.response.receiveAt
-      })
-    case ActionTypes.LOGIN_CLIENT_SUCCESS:
-      if (action.response && action.response.data && action.response.data._id && action.response.token) {
-        cookie.save('user_id', action.response.data._id, { path: '/' });
-        cookie.save('user_token', action.response.token, { path: '/' });
-      }
-      return Object.assign({}, state, {
-        isFetching: false,
-        didInvalidate: true,
-        data: (action.response && action.response.data && isClient(action.response.data)) ? action.response.data : null,
-        lastUpdated: action.response && action.response.receivedAt
-      })
-    case ActionTypes.USER_TOKEN_FAILURE:
-      cookie.remove('user_id', { path: '/' });
-      cookie.remove('user_token', { path: '/' });
-      return state
-    case ActionTypes.USER_DESTROY:
-      return Object.assign({}, state, {
-        isFetching: false,
-        didInvalidate: true,
-        data: null,
-        lastUpdated: undefined
-      })
-    default:
-      return state
-  }
-}
 
 const config = (state = {
   isFetching: false,
--- reducers/user.js
+++ reducers/user.js
@@ -1,12 +1,14 @@
+import cookie from 'react-cookie';
 import * as ActionTypes from '../actions';
 import { combineReducers } from 'redux';
+import { isClient } from '../core/util';
 
 const devices = (state = null, action) => {
   switch (action.type) {
     case ActionTypes.USER_DEVICES_EDIT_SUCCESS:
       return Object.assign({}, state, action.response.data);
     default:
-      return state;
+      return (action.response.data && action.response.data.devices) || state;
   }
 }
 
@@ -15,7 +17,7 @@ const experiences = (state = null, action) => {
     case ActionTypes.USER_EXPERIENCES_EDIT_SUCCESS:
       return Object.assign({}, state, action.response.data);
     default:
-      return state;
+      return (action.response.data && action.response.data.experiences) || state;
   }
 }
 
@@ -24,7 +26,7 @@ const educations = (state = null, action) => {
     case ActionTypes.USER_EDUCATIONS_EDIT_SUCCESS:
       return Object.assign({}, state, action.response.data);
     default:
-      return state;
+      return (action.response.data && action.response.data.educations) || state;
   }
 }
 
@@ -33,7 +35,7 @@ const achievements = (state = null, action) => {
     case ActionTypes.USER_ACHIEVEMENTS_EDIT_SUCCESS:
       return Object.assign({}, state, action.response.data);
     default:
-      return state;
+      return (action.response.data && action.response.data.achievements) || state;
   }
 }
 
@@ -42,7 +44,7 @@ const reviews = (state = null, action) => {
     case ActionTypes.USER_REVIEWS_EDIT_SUCCESS:
       return Object.assign({}, state, action.response.data);
     default:
-      return state;
+      return (action.response.data && action.response.data.reviews) || state;
   }
 }
 
@@ -51,10 +53,10 @@ const schedules = (state = null, action) => {
     case ActionTypes.USER_SCHEDULES_EDIT_SUCCESS:
       return action.booker;
     default:
-      return state;
+      return (action.response.data && action.response.data.schedules) || state;
   }
 }
-const user = combineReducers({
+const userData = combineReducers({
   devices,
   experiences,
   educations,
@@ -63,9 +65,95 @@ const user = combineReducers({
   schedules
 })
 
-const extendedUser = (state, action) => ({
-  ...state,
-  ...user(state, action),
-})
+const extendedUserData = (state, action) => {
+  switch (action.type) {
+    case ActionTypes.USER_DEVICES_EDIT_SUCCESS:
+    case ActionTypes.USER_EXPERIENCES_EDIT_SUCCESS:
+    case ActionTypes.USER_EDUCATIONS_EDIT_SUCCESS:
+    case ActionTypes.USER_ACHIEVEMENTS_EDIT_SUCCESS:
+    case ActionTypes.USER_REVIEWS_EDIT_SUCCESS:
+    case ActionTypes.USER_SCHEDULES_EDIT_SUCCESS:
+      return Object.assign({}, state, userData(state, action));
+    default:
+      return state;
+  }
+}
 
-export default extendedUser;
+const user = (state = {
+  isFetching: false,
+  didInvalidate: true,
+  data: null
+}, action) => {
+  switch (action.type) {
+    case ActionTypes.USER_REQUEST:
+    case ActionTypes.USER_TOKEN_REQUEST:
+    case ActionTypes.USER_CREATE_REQUEST:
+    case ActionTypes.USER_EDIT_REQUEST:
+    case ActionTypes.USER_EDUCATION_EDIT_REQUEST:
+    case ActionTypes.LOGIN_REQUEST:
+    case ActionTypes.LOGIN_CLIENT_REQUEST:
+      return Object.assign({}, state, {
+        isFetching: true,
+        didInvalidate: false
+      })
+    case ActionTypes.USER_SUCCESS:
+    case ActionTypes.USER_TOKEN_SUCCESS:
+    case ActionTypes.USER_CREATE_SUCCESS:
+    case ActionTypes.LOGIN_SUCCESS:
+      if (action.response && action.response.data && action.response.data._id && action.response.token) {
+        cookie.save('user_id', action.response.data._id, { path: '/' });
+        cookie.save('user_token', action.response.token, { path: '/' });
+      }
+      return Object.assign({}, state, {
+        isFetching: false,
+        didInvalidate: false,
+        data: action.response && action.response.data,
+        lastUpdated: action.response && action.response.receivedAt
+      })
+    case ActionTypes.USER_EDIT_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        didInvalidate: false,
+        data: action.response && action.response.data,
+        lastUpdated: action.response && action.response.receivedAt
+      })
+    case ActionTypes.USER_DEVICES_EDIT_SUCCESS:
+    case ActionTypes.USER_EXPERIENCES_EDIT_SUCCESS:
+    case ActionTypes.USER_EDUCATIONS_EDIT_SUCCESS:
+    case ActionTypes.USER_ACHIEVEMENTS_EDIT_SUCCESS:
+    case ActionTypes.USER_REVIEWS_EDIT_SUCCESS:
+    case ActionTypes.USER_SCHEDULES_EDIT_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        didInvalidate: false,
+        data: extendedUserData(state, action),
+        lastUpdated: action.response && action.response.receiveAt
+      })
+    case ActionTypes.LOGIN_CLIENT_SUCCESS:
+      if (action.response && action.response.data && action.response.data._id && action.response.token) {
+        cookie.save('user_id', action.response.data._id, { path: '/' });
+        cookie.save('user_token', action.response.token, { path: '/' });
+      }
+      return Object.assign({}, state, {
+        isFetching: false,
+        didInvalidate: true,
+        data: (action.response && action.response.data && isClient(action.response.data)) ? action.response.data : null,
+        lastUpdated: action.response && action.response.receivedAt
+      })
+    case ActionTypes.USER_TOKEN_FAILURE:
+      cookie.remove('user_id', { path: '/' });
+      cookie.remove('user_token', { path: '/' });
+      return state
+    case ActionTypes.USER_DESTROY:
+      return Object.assign({}, state, {
+        isFetching: false,
+        didInvalidate: true,
+        data: null,
+        lastUpdated: undefined
+      })
+    default:
+      return state
+  }
+}
+
+export default user;

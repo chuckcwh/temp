c87b9865da88d944485f3811d06b3a8757579de5
Eric Lu <ericluwj@hotmail.com>
2016-09-07 11:37:42 +0800

    Simplified user reducer with new normalize utility function.

--- reducers/user.js
+++ reducers/user.js
@@ -1,7 +1,7 @@
 import cookie from 'react-cookie';
 import * as ActionTypes from '../actions';
 import { combineReducers } from 'redux';
-import { isClient } from '../core/util';
+import { normalize, isClient } from '../core/util';
 
 const devices = (state = null, action) => {
   switch (action.type) {
@@ -11,15 +11,9 @@ const devices = (state = null, action) => {
     case ActionTypes.LOGIN_SUCCESS:
     case ActionTypes.LOGIN_CLIENT_SUCCESS:
     case ActionTypes.USER_EDIT_SUCCESS:
-      return Object.assign({}, state, action.response.data && action.response.data.devices && action.response.data.devices.reduce((result, elem) => {
-        result[elem._id] = elem;
-        return result;
-      }, {}));
+      return Object.assign({}, state, normalize(action.response.data && action.response.data.devices);
     case ActionTypes.USER_DEVICES_SUCCESS:
-      return Object.assign({}, state, action.response.data.reduce((result, elem) => {
-        result[elem._id] = elem;
-        return result;
-      }, {}));
+      return Object.assign({}, state, normalize(action.response.data));
     case ActionTypes.USER_DEVICE_SUCCESS:
     case ActionTypes.USER_DEVICE_CREATE_SUCCESS:
     case ActionTypes.USER_DEVICE_EDIT_SUCCESS:
@@ -39,15 +33,9 @@ const experiences = (state = null, action) => {
     case ActionTypes.LOGIN_SUCCESS:
     case ActionTypes.LOGIN_CLIENT_SUCCESS:
     case ActionTypes.USER_EDIT_SUCCESS:
-      return Object.assign({}, state, action.response.data && action.response.data.experiences && action.response.data.experiences.reduce((result, elem) => {
-        result[elem._id] = elem;
-        return result;
-      }, {}));
+      return Object.assign({}, state, normalize(action.response.data && action.response.data.experiences));
     case ActionTypes.USER_EXPERIENCES_SUCCESS:
-      return Object.assign({}, state, action.response.data.reduce((result, elem) => {
-        result[elem._id] = elem;
-        return result;
-      }, {}));
+      return Object.assign({}, state, normalize(action.response.data));
     case ActionTypes.USER_EXPERIENCE_SUCCESS:
     case ActionTypes.USER_EXPERIENCE_CREATE_SUCCESS:
     case ActionTypes.USER_EXPERIENCE_EDIT_SUCCESS:
@@ -67,15 +55,9 @@ const educations = (state = null, action) => {
     case ActionTypes.LOGIN_SUCCESS:
     case ActionTypes.LOGIN_CLIENT_SUCCESS:
     case ActionTypes.USER_EDIT_SUCCESS:
-      return Object.assign({}, state, action.response.data && action.response.data.educations && action.response.data.educations.reduce((result, elem) => {
-        result[elem._id] = elem;
-        return result;
-      }, {}));
+      return Object.assign({}, state, normalize(action.response.data && action.response.data.educations));
     case ActionTypes.USER_EDUCATIONS_SUCCESS:
-      return Object.assign({}, state, action.response.data.reduce((result, elem) => {
-        result[elem._id] = elem;
-        return result;
-      }, {}));
+      return Object.assign({}, state, normalize(action.response.data));
     case ActionTypes.USER_EDUCATION_SUCCESS:
     case ActionTypes.USER_EDUCATION_CREATE_SUCCESS:
     case ActionTypes.USER_EDUCATION_EDIT_SUCCESS:
@@ -95,15 +77,9 @@ const achievements = (state = null, action) => {
     case ActionTypes.LOGIN_SUCCESS:
     case ActionTypes.LOGIN_CLIENT_SUCCESS:
     case ActionTypes.USER_EDIT_SUCCESS:
-      return Object.assign({}, state, action.response.data && action.response.data.achievements && action.response.data.achievements.reduce((result, elem) => {
-        result[elem._id] = elem;
-        return result;
-      }, {}));
+      return Object.assign({}, state, normalize(action.response.data && action.response.data.achievements));
     case ActionTypes.USER_ACHIEVEMENTS_SUCCESS:
-      return Object.assign({}, state, action.response.data.reduce((result, elem) => {
-        result[elem._id] = elem;
-        return result;
-      }, {}));
+      return Object.assign({}, state, normalize(action.response.data));
     case ActionTypes.USER_ACHIEVEMENT_SUCCESS:
     case ActionTypes.USER_ACHIEVEMENT_CREATE_SUCCESS:
     case ActionTypes.USER_ACHIEVEMENT_EDIT_SUCCESS:
@@ -123,15 +99,9 @@ const reviews = (state = null, action) => {
     case ActionTypes.LOGIN_SUCCESS:
     case ActionTypes.LOGIN_CLIENT_SUCCESS:
     case ActionTypes.USER_EDIT_SUCCESS:
-      return Object.assign({}, state, action.response.data && action.response.data.reviews && action.response.data.reviews.reduce((result, elem) => {
-        result[elem._id] = elem;
-        return result;
-      }, {}));
+      return Object.assign({}, state, normalize(action.response.data && action.response.data.reviews));
     case ActionTypes.USER_REVIEWS_SUCCESS:
-      return Object.assign({}, state, action.response.data.reduce((result, elem) => {
-        result[elem._id] = elem;
-        return result;
-      }, {}));
+      return Object.assign({}, state, normalize(action.response.data));
     case ActionTypes.USER_REVIEW_SUCCESS:
     case ActionTypes.USER_REVIEW_CREATE_SUCCESS:
     case ActionTypes.USER_REVIEW_EDIT_SUCCESS:
@@ -151,15 +121,9 @@ const schedules = (state = null, action) => {
     case ActionTypes.LOGIN_SUCCESS:
     case ActionTypes.LOGIN_CLIENT_SUCCESS:
     case ActionTypes.USER_EDIT_SUCCESS:
-      return Object.assign({}, state, action.response.data && action.response.data.schedules && action.response.data.schedules.reduce((result, elem) => {
-        result[elem._id] = elem;
-        return result;
-      }, {}));
+      return Object.assign({}, state, normalize(action.response.data && action.response.data.schedules));
     case ActionTypes.USER_SCHEDULES_SUCCESS:
-      return Object.assign({}, state, action.response.data.reduce((result, elem) => {
-        result[elem._id] = elem;
-        return result;
-      }, {}));
+      return Object.assign({}, state, normalize(action.response.data));
     case ActionTypes.USER_SCHEDULE_SUCCESS:
     case ActionTypes.USER_SCHEDULE_CREATE_SUCCESS:
     case ActionTypes.USER_SCHEDULE_EDIT_SUCCESS:
@@ -322,7 +286,14 @@ const user = (state = {
       return Object.assign({}, state, {
         isFetching: false,
         didInvalidate: true,
-        data: null,
+        data: {
+          devices,
+          experiences,
+          educations,
+          achievements,
+          reviews,
+          schedules
+        },
         lastUpdated: undefined
       })
     default:
